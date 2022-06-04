73a03691aa7b48353288b26e685e2f41245475ec
Aldovani <aldovanihcosta@gmail.com>
2022-06-04 01:03:04 -0300

    update

--- app/Controllers/Http/inscribe/Classe/Main.ts
+++ app/Controllers/Http/inscribe/Classe/Main.ts
@@ -1,21 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { Classe } from 'App/Models'
-import StoreValidator from 'App/Validators/inscribe/Classe/StoreValidator'
+// import { Classe } from 'App/Models'
+// import StoreValidator from 'App/Validators/inscribe/Classe/StoreValidator'
 
 export default class MainsController {
   public async show({ auth, params }: HttpContextContract) {
     const user = auth.user!
     await user.load('courseSubscription', (query) => {
       query.where('course_id', '=', params.id)
-      query.preload('classes', (query) => {})
+      // query.preload('classes', (query) => {})
     })
 
     return user
   }
 
-  public async store({ auth, request }: HttpContextContract) {
-    const { classeId } = await request.validate(StoreValidator)
-    const user = auth.user!
-    const course = await Classe.findOrFail(classeId)
+  public async store({}: HttpContextContract) {
+    // const { classeId } = await request.validate(StoreValidator)
+    // const user = auth.user!
+    // const course = await Classe.findOrFail(classeId)
   }
 }
