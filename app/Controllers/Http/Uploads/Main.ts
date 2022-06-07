a4c670fb15282a10a2924025357102e794f72ddd
Aldovani <aldovanihcosta@gmail.com>
2022-06-07 01:06:20 -0300

    Update upload

--- app/Controllers/Http/Uploads/Main.ts
+++ app/Controllers/Http/Uploads/Main.ts
@@ -3,22 +3,7 @@ import Application from '@ioc:Adonis/Core/Application'
 import { File } from 'App/Models'
 
 export default class UploadsController {
-  public async show({ response, params, auth }: HttpContextContract) {
-    const arquivo = await (
-      await File.query().where({ fileName: params.file }).firstOrFail()
-    ).fileCategory
-    if (arquivo === 'course_video') {
-      const user = await auth.authenticate()
-      await user.load('stripeCustomers')
-      const { stripeCustomers } = user
-      if (stripeCustomers.status !== 'active') {
-        return response.unauthorized({
-          error: { message: 'Você não tem permissão para acessar este arquivo' },
-        })
-      }
-
-      return response.download(Application.tmpPath(`uploads`, params.file))
-    }
+  public async show({ response, params }: HttpContextContract) {
     return response.download(Application.tmpPath(`uploads`, params.file))
   }
 }
